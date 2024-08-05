5edecdc87fb0140adc9bd9cecf95c1264aa21443
Vinicius Luis <viniciusluisdossantos@gmail.com>
2024-08-05 19:55:06 -0300

    up

--- Plascas3D_parede.py
+++ Plascas3D_parede.py
@@ -4,7 +4,7 @@
 from time import sleep
 
 def linha():
-    print('-='*20)
+    print('-='*30)
 
 def caculo(altura, largura):
     return altura * largura
@@ -20,25 +20,35 @@ def obter_valor(descricao):
             print(f"Valor incorreto! Por favor, digite um número válido para {descricao}.")
 linha()
 
-altura_placa = obter_valor("Qual a altura da placa")
-largura_placa = obter_valor("Qual a largura da placa")
-
-linha()
-
 while True:
-    resp = input(f'A placa tem as dimenções {altura_placa}X{largura_placa} correto? [S/N] ').strip().upper()[0]
+    altura_placa = obter_valor("Qual a altura da placa [m.cm]")
+    largura_placa = obter_valor("Qual a largura da placa [m.cm]")
+    linha()
+    resp = input(f'A placa tem as dimenções {altura_placa:.2f}X{largura_placa:.2f} correto? [S/N] ').strip().upper()[0]
     if resp in 'S':
         break
+        
     elif resp != 'N':
         print('Resposta errada digite S ou N para processeguir!')
-        sleep(2)    
+        sleep(2)
+    
 linha()
-
-sleep(1)
 print('Agora preciso das medidas da parede')
+while True:
+    sleep(1)
+    altura_parede = obter_valor("Qual a altura da parede [m.cm]")
+    largura_parede = obter_valor("Qual a largura da parede [m.cm]")
+    linha()
+    
+    resp = input(f'A parede tem as dimenções {altura_parede:.2f}X{largura_parede:.2f} correto? [S/N] ').strip().upper()[0]
+    if resp in 'S':
+        break
+        
+    elif resp != 'N':
+        print('Resposta errada digite S ou N para processeguir!')
+        sleep(2)
 
-altura_parede = obter_valor("Qual a altura da parede")
-largura_parede = obter_valor("Qual a largura da parede")
+
 
 linha()
 
@@ -46,7 +56,7 @@ area_parede = caculo(altura_parede, largura_parede)
 area_placa = caculo(altura_placa, largura_placa)
 quantidade_minima_placa = area_parede / area_placa
 
-print(f'Para revestir uma parede de {area_parede:.2f}m² com placas de {area_placa:.2f}m², é preciso comprar no minimo {quantidade_minima_placa:.0f}m².')
+print(f'Para revestir uma parede de {area_parede:.2f}m² com placas de {area_placa:.2f}m², é preciso comprar no minimo "{quantidade_minima_placa:.0f}" Placas.')
 print(f'Observação: Bom comprar quantidade a mais para alguns casos de acabamento.')
 linha()
 print('FIM!')
